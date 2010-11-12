8d83d8afcd4095249c49a1db76ebb1d89f3d4e6b
ck <c_kettenbach@hotmail.com>
2010-11-11 18:28:30 -0800

    first commit

---
+++ src/Core/UnityWrapper.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+
+
+namespace UnityDemo.Core
+{
+    public class UnityWrapper : IIoCBootstrap
+    {
+        private static IUnityContainer _container;
+
+        public static UnityWrapper Create()
+        {
+            return new UnityWrapper();
+        }
+
+        public UnityWrapper()
+        {
+            if(_container == null)
+            {
+                Init();
+            }
+            
+        }
+
+        public IUnityContainer Container
+        {
+            get { return _container; }
+        }
+
+
+        public void Register<TIn, TOut>() where TOut:TIn
+        {
+            _container.RegisterType<TIn, TOut>();
+        }
+
+
+        public void Init()
+        {
+            var fileMap = new ExeConfigurationFileMap { ExeConfigFilename = @"C:\workspace\new\UnityDemo-v1.0.0.1\src\Core\unity.config" };
+            
+            Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+            var unitySection = (UnityConfigurationSection)configuration.GetSection("unity");
+            _container = new UnityContainer().LoadConfiguration(unitySection);
+        }
+    }
+}
